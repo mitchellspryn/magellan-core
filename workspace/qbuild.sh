13ec7c02a935a3b09578c53270021ab515548fa0
Mitchell Spryn <mitchell.spryn@gmail.com>
2019-06-30 18:47:55 -0700

    Make build script output debug symbols.

--- workspace/qbuild.sh
+++ workspace/qbuild.sh
@@ -7,6 +7,6 @@
 if [ "$sourced" == "0" ]; then
     echo "Run this script with 'source ./qbuild.sh'"
 else
-    catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+    catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so -DCMAKE_BUILD_TYPE=Debug
     source devel/setup.bash
 fi
--- workspace/rebuild.sh
+++ workspace/rebuild.sh
@@ -10,6 +10,6 @@ else
     rm -r build/
     rm -r devel/
     catkin_make clean
-    catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+    catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so -DCMAKE_BUILD_TYPE=Debug
     source devel/setup.bash
 fi
