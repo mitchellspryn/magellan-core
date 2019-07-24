149c7d2bebf357b23254a7ccfd6a7670e77ee44a
Mitchell Spryn <mitchell.spryn@gmail.com>
2019-07-23 21:14:09 -0700

    Working multi-stream example

--- workspace/src/control_webpage/scripts/control_webpage.py
+++ workspace/src/control_webpage/scripts/control_webpage.py
@@ -5,13 +5,16 @@ import rospy
 import signal
 import warnings
 import cv2
+import threading
+import base64
 
 # Upon termination, bottle seems to complain about a resource warning
 # It doesn't seem to affect anything, so ignore it
 #warnings.simplefilter("ignore", ResourceWarning)
 
 status_webpage = None
-video_capture = None
+dummy_image = None
+dummy_background_thread = None
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -24,21 +27,53 @@ def static_file(filename):
     print(filename)
     return flask.send_from_directory(os.getcwd(), filename)
 
-def image_generator():
-    global video_capture
+def background_dummy_thread():
+    global dummy_image
+    video_capture = cv2.VideoCapture(0)
+    while(True):
+        ret, frame = video_capture.read()
+        ret, jpg = cv2.imencode('.jpeg', frame)
+        dummy_image = base64.b64encode(jpg).decode('utf-8')
+        #dummy_image = jpg.tobytes()
+
+def dummy_image_generator():
+    global dummy_image
     while(True):
         try:
-            ret, frame = video_capture.read()
-            ret, jpg = cv2.imencode('.jpg', frame)
-            jpg_bytes = jpg.tobytes()
-            yield(b'--frame\r\nContent-Type:image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n\r\n')
+            if (dummy_image is not None):
+                yield(b'--frame\r\nContent-Type:image/jpeg\r\n\r\n' + dummy_image + b'\r\n\r\n')
         except Exception as e:
             print(str(e))
 
+def get_single_dummy_image():
+    if (dummy_image is not None):
+        return flask.jsonify({'data': 'data:image/jpeg;charset=utf-8;base64,' + dummy_image})
+    return flask.jsonify({'data': 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="'})
 
-@app.route('/image')
+@app.route('/bottom-camera-image')
+def bottom_camera_image():
+    #return flask.Response(dummy_image_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return get_single_dummy_image()
+
+@app.route('/xtion-rgb-image')
+def xtion_rgb_image():
+    #return flask.Response(dummy_image_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return get_single_dummy_image()
+
+@app.route('/xtion-depth-image')
+def xtion_depth_image():
+    #return flask.Response(dummy_image_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return get_single_dummy_image()
+
+@app.route('/lidar-image')
+def lidar_image():
+    #return flask.Response(dummy_image_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return get_single_dummy_image()
+
+@app.route('/dummy-image')
 def image():
-    return flask.Response(image_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    #return flask.Response(dummy_image_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return get_single_dummy_image()
 
 # Use this to cleanly kill the webpage when ctrl+c is pressed
 def sig_handler(sig, frame):
@@ -50,7 +85,10 @@ def main():
     #TODO: ros stuff
     rospy.init_node('control_webpage')
 
-    video_capture = cv2.VideoCapture(0)
+    dummy_background_thread = threading.Thread(target=background_dummy_thread, args=())
+    dummy_background_thread.daemon = True
+    dummy_background_thread.start()
+
     signal.signal(signal.SIGINT, sig_handler)
 
     with open('control_webpage.html', 'r') as f:
