2d5eb3701eaf993afd63ca3ba2b65dec1df4062d
Mitchell Spryn <mitchell.spryn@gmail.com>
2020-06-06 15:16:00 -0700

    Update zed sensor reader to support writing and reading from svo.

--- workspace/src/zed_sensor_reader/src/zed_sensor_reader.cpp
+++ workspace/src/zed_sensor_reader/src/zed_sensor_reader.cpp
@@ -18,6 +18,7 @@
 #include <sl/Camera.hpp>
 #include <std_msgs/ColorRGBA.h>
 #include <stdexcept>
+#include <string>
 #include <thread>
 #include <unistd.h>
 #include <visualization_msgs/Marker.h>
@@ -38,11 +39,98 @@ typedef struct capture_parameters
     bool publish_sensors;
     bool publish_pose;
     bool enable_area_memory;
+    bool fill_in_camera_pixels;
+    bool fill_in_confidence;
+    bool use_external_clock;
     int frames_per_second;
+    std::string save_file_name;
+    std::string load_file_name;
 } capture_parameters_t;
 
 static std::string g_frame_id = "map";
 static constexpr int g_pose_update_rate = 60;
+
+std::string sl_err_to_string(sl::ERROR_CODE error_code)
+{
+    switch (error_code)
+    {
+        case sl::ERROR_CODE::SUCCESS:
+            return "SUCCESS";
+        case sl::ERROR_CODE::FAILURE:
+            return "FAILURE";
+        case sl::ERROR_CODE::NO_GPU_COMPATIBLE:
+            return "NO_GPU_COMPATIBLE";
+        case sl::ERROR_CODE::NOT_ENOUGH_GPU_MEMORY:
+            return "NOT_ENOUGH_GPU_MEMORY";
+        case sl::ERROR_CODE::CAMERA_NOT_DETECTED:
+            return "CAMERA_NOT_DETECTED";
+        case sl::ERROR_CODE::SENSORS_NOT_AVAILABLE:
+            return "SENSORS_NOT_AVAILABLE";
+        case sl::ERROR_CODE::INVALID_RESOLUTION:
+            return "INVALID_RESOLUTION";
+        case sl::ERROR_CODE::LOW_USB_BANDWIDTH:
+            return "LOW_USB_BANDWIDTH";
+        case sl::ERROR_CODE::CALIBRATION_FILE_NOT_AVAILABLE:
+            return "CALIBRATION_FILE_NOT_AVAILABLE";
+        case sl::ERROR_CODE::INVALID_CALIBRATION_FILE:
+            return "INVALID_CALIBRATION_FILE";
+        case sl::ERROR_CODE::INVALID_SVO_FILE:
+            return "INVALID_SVO_FILE";
+        case sl::ERROR_CODE::SVO_RECORDING_ERROR:
+            return "SVO_RECORDING_ERROR";
+        case sl::ERROR_CODE::SVO_UNSUPPORTED_COMPRESSION:
+            return "SVO_UNSUPPORTED_COMPRESSION";
+        case sl::ERROR_CODE::END_OF_SVOFILE_REACHED:
+            return "END_OF_SVOFILE_REACHED";
+        case sl::ERROR_CODE::INVALID_COORDINATE_SYSTEM:
+            return "INVALID_COORDINATE_SYSTEM";
+        case sl::ERROR_CODE::INVALID_FIRMWARE:
+            return "INVALID_FIRMWARE";
+        case sl::ERROR_CODE::INVALID_FUNCTION_PARAMETERS:
+            return "INVALID_FUNCTION_PARAMETERS";
+        case sl::ERROR_CODE::CUDA_ERROR:
+            return "CUDA_ERROR";
+        case sl::ERROR_CODE::CAMERA_NOT_INITIALIZED:
+            return "CAMERA_NOT_INITIALIZED";
+        case sl::ERROR_CODE::NVIDIA_DRIVER_OUT_OF_DATE:
+            return "NVIDIA_DRIVER_OUT_OF_DATE";
+        case sl::ERROR_CODE::INVALID_FUNCTION_CALL:
+            return "INVALID_FUNCTION_CALL";
+        case sl::ERROR_CODE::CORRUPTED_SDK_INSTALLATION:
+            return "CORRUPTED_SDK_INSTALLATION";
+        case sl::ERROR_CODE::INCOMPATIBLE_SDK_VERSION:
+            return "INCOMPATIBLE_SDK_VERSION";
+        case sl::ERROR_CODE::INVALID_AREA_FILE:
+            return "INVALID_AREA_FILE";
+        case sl::ERROR_CODE::INCOMPATIBLE_AREA_FILE:
+            return "INCOMPATIBLE_AREA_FILE";
+        case sl::ERROR_CODE::CAMERA_FAILED_TO_SETUP:
+            return "CAMERA_FAILED_TO_SETUP";
+        case sl::ERROR_CODE::CAMERA_DETECTION_ISSUE:
+            return "CAMERA_DETECTION_ISSUE";
+        case sl::ERROR_CODE::CANNOT_START_CAMERA_STREAM:
+            return "CANNOT_START_CAMERA_STREAM";
+        case sl::ERROR_CODE::NO_GPU_DETECTED:
+            return "NO_GPU_DETECTED";
+        case sl::ERROR_CODE::PLANE_NOT_FOUND:
+            return "PLANE_NOT_FOUND";
+        case sl::ERROR_CODE::MODULE_NOT_COMPATIBLE_WITH_CAMERA:
+            return "MODULE_NOT_COMPATIBLE_WITH_CAMERA";
+        case sl::ERROR_CODE::MOTION_SENSORS_REQUIRED:
+            return "MOTION_SENSORS_REQUIRED";
+        case sl::ERROR_CODE::LAST:
+            return "LAST";
+        default:
+            throw std::runtime_error("Unrecognized sl::ERROR_CODE.");
+    }
+}
+
+ros::Time sl_time_to_ros_time(sl::Timestamp t) 
+{
+    uint32_t sec = static_cast<uint32_t>(t.getNanoseconds() / 1000000000);
+    uint32_t nsec = static_cast<uint32_t>(t.getNanoseconds() % 1000000000);
+    return ros::Time(sec, nsec);
+}
 
 inline void sl_vec3_to_ros_vec3(const sl::float3 &sl_data, geometry_msgs::Vector3 &ros_data)
 {
@@ -106,7 +194,7 @@ void initialize_point_cloud_msg(sensor_msgs::PointCloud2 &point_cloud_msg, const
     point_cloud_msg.fields.push_back(make_point_field("r", 12, 2)); //Uint8
     point_cloud_msg.fields.push_back(make_point_field("g", 13, 2)); 
     point_cloud_msg.fields.push_back(make_point_field("b", 14, 2)); 
-    point_cloud_msg.fields.push_back(make_point_field("a", 15, 2)); 
+    point_cloud_msg.fields.push_back(make_point_field("confidence", 15, 2)); 
 
     point_cloud_msg.data.resize(point_step * width * height);
 
@@ -122,6 +210,8 @@ void initialize_point_cloud_msg(sensor_msgs::PointCloud2 &point_cloud_msg, const
 void image_pose_grab_thread(const capture_parameters_t &capture_parameters)
 {
     sl::Mat point_cloud;
+    sl::Mat image;
+    sl::Mat confidence;
     sl::Pose pose;
 
     sensor_msgs::PointCloud2 point_cloud_msg;
@@ -143,6 +233,8 @@ void image_pose_grab_thread(const capture_parameters_t &capture_parameters)
     }
     pose_msg.confidence = -1;
 
+    bool is_recording = capture_parameters.save_file_name.size() > 0;
+    bool is_from_recording = capture_parameters.load_file_name.size() > 0;
 
     const int frame_downsample_counter = g_pose_update_rate / capture_parameters.frames_per_second;
     int frame_counter = 0;
@@ -150,15 +242,77 @@ void image_pose_grab_thread(const capture_parameters_t &capture_parameters)
     while (ros::ok())
     {
         // TODO: how much to sleep?
-        if (g_camera.grab(g_runtime_parameters) == sl::ERROR_CODE::SUCCESS)
+        sl::ERROR_CODE err = g_camera.grab(g_runtime_parameters);
+        if (err == sl::ERROR_CODE::SUCCESS)
         {
             ros::Time now = ros::Time::now();
+
+            if (is_from_recording)
+            {
+                ros::Time image_time = sl_time_to_ros_time(g_camera.getTimestamp(sl::TIME_REFERENCE::IMAGE));
+
+                if (capture_parameters.use_external_clock)
+                {
+                    while (image_time > now)
+                    {
+                        ros::Duration(0.001).sleep();
+                        now = ros::Time::now();
+                    }
+                }
+                else
+                {
+                    now = image_time;
+                }
+                
+            }
 
             if (frame_counter == 0)
             {
                 point_cloud_msg.header.stamp = now;
                 g_camera.retrieveMeasure(point_cloud, sl::MEASURE::XYZRGBA);
                 memcpy(static_cast<void*>(&point_cloud_msg.data[0]), point_cloud.getPtr<sl::float4>(), height*width*4*sizeof(float));
+
+                if (capture_parameters.fill_in_camera_pixels) 
+                {
+                    g_camera.retrieveImage(image, sl::VIEW::LEFT);
+                    unsigned char* point_cloud_data = static_cast<unsigned char*>(&point_cloud_msg.data[0]);
+                    sl::float3* image_data = image.getPtr<sl::float3>();
+
+                    point_cloud_data += 12;
+
+                    for (int y = 0; y < height; y++) 
+                    {
+                        for (int x = 0; x < width; x++) 
+                        {
+                            *point_cloud_data++ = image_data->r;
+                            *point_cloud_data++ = image_data->g;
+                            *point_cloud_data++ = image_data->b;
+
+                            point_cloud_data += 13;
+                            image_data++;
+                        }
+                    }
+                }
+
+                if (capture_parameters.fill_in_confidence)
+                {
+                    g_camera.retrieveMeasure(confidence, sl::MEASURE::CONFIDENCE);
+
+                    unsigned char* point_cloud_data = static_cast<unsigned char*>(&point_cloud_msg.data[0]);
+                    sl::float1* confidence_data = confidence.getPtr<sl::float1>();
+
+                    point_cloud_data += 15;
+
+                    for (int y = 0; y< height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            *point_cloud_data = static_cast<unsigned char>(100.0f - (*confidence_data));
+                            point_cloud_data += 16;
+                        }
+                    }
+                }
+
                 g_point_cloud_publisher.publish(point_cloud_msg);
             }
 
@@ -201,6 +355,10 @@ void image_pose_grab_thread(const capture_parameters_t &capture_parameters)
             frame_counter++;
             frame_counter %= frame_downsample_counter;
         }
+        else if (err == sl::ERROR_CODE::END_OF_SVOFILE_REACHED)
+        {
+            ROS_ERROR("End of SVO file reached.");
+        }
 
         ros::spinOnce();
     }
@@ -216,6 +374,13 @@ void sensor_grab_thread(const capture_parameters_t &capture_parameters)
     magellan_messages::MsgZedSensors zed_sensor_msg;
     zed_sensor_msg.header.frame_id = g_frame_id;
 
+    bool is_from_recording = (capture_parameters.load_file_name.size() > 0);
+
+    if (is_from_recording)
+    {
+        throw std::runtime_error("Cannot replay sensor stream from recording.");
+    }
+
     for (int i = 0; i < 9; i++)
     {
         zed_sensor_msg.imu.angular_velocity_covariance[i] = 0;
@@ -229,9 +394,11 @@ void sensor_grab_thread(const capture_parameters_t &capture_parameters)
         // TODO: how much to sleep?
         if (g_camera.getSensorsData(sensors_data, sl::TIME_REFERENCE::CURRENT) == sl::ERROR_CODE::SUCCESS)
         {
+            ros::Time now = ros::Time::now();
+
             if (sensors_data.imu.timestamp > last_collected_timestamp)
             {
-                zed_sensor_msg.header.stamp = ros::Time::now();
+                zed_sensor_msg.header.stamp = now;
 
                 sl_vec3_to_ros_vec3(sensors_data.imu.angular_velocity, zed_sensor_msg.imu.angular_velocity);
                 sl_cov_mat_to_ros_cov_mat(sensors_data.imu.angular_velocity_covariance, zed_sensor_msg.imu.angular_velocity_covariance);
@@ -257,6 +424,7 @@ void sensor_grab_thread(const capture_parameters_t &capture_parameters)
 sl::ERROR_CODE init_camera(const capture_parameters_t &parameters)
 {
     sl::InitParameters init_parameters;
+
     init_parameters.camera_fps = 60; // Maximum accuracy for pose calculations. Will downsample for point cloud.
     init_parameters.camera_resolution = sl::RESOLUTION::HD720;
     init_parameters.coordinate_system = sl::COORDINATE_SYSTEM::LEFT_HANDED_Z_UP;
@@ -299,10 +467,13 @@ int main(int argc, char** argv)
     capture_parameters.publish_pose = false;
     capture_parameters.publish_sensors = false;
     capture_parameters.enable_area_memory = true;
+    capture_parameters.use_external_clock = false;
+    capture_parameters.save_file_name = "";
+    capture_parameters.load_file_name = "";
 
     // TODO: args
     int c;
-    while ((c = getopt(argc, argv, "dpsf:")) != -1)
+    while ((c = getopt(argc, argv, "dpsf:o:l:e")) != -1)
     {
         switch (c)
         {
@@ -314,6 +485,9 @@ int main(int argc, char** argv)
                 break;
             case 's':
                 capture_parameters.publish_sensors = true;
+                break;
+            case 'e':
+                capture_parameters.use_external_clock = true;
                 break;
             case 'f':
                 try
@@ -328,6 +502,11 @@ int main(int argc, char** argv)
                     return 1;
                 }
                 break;
+            case 'o':
+                capture_parameters.save_file_name = std::string(optarg);
+                break;
+            case 'l':
+                capture_parameters.load_file_name = std::string(optarg);
             default:
                 ROS_FATAL("Unknown command line flag: %c.", c);
                 return 1;
@@ -342,12 +521,31 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    if (capture_parameters.load_file_name.size() > 0 && capture_parameters.save_file_name.size() > 0)
+    {
+        ROS_FATAL("Error: requested both save and load file.");
+        return 1;
+    }
 
     sl::ERROR_CODE open_result = init_camera(capture_parameters);
     if (open_result != sl::ERROR_CODE::SUCCESS)
     {
-        ROS_FATAL("Could not open camera. Open returns %d.", static_cast<int>(open_result));
+        ROS_FATAL("Could not open camera. Open returns %s.", sl_err_to_string(open_result).c_str());
         return 1;
+    }
+
+    if (capture_parameters.save_file_name.size() > 0)
+    {
+        sl::ERROR_CODE save_result = g_camera.enableRecording(
+                sl::RecordingParameters(capture_parameters.save_file_name.c_str(), sl::SVO_COMPRESSION_MODE::H265)); //TODO: Should I use lossless and take the CPU hit?
+
+        if (save_result != sl::ERROR_CODE::SUCCESS)
+        {
+            ROS_FATAL("Could not open file %s for saving. API returns %s.", 
+                    capture_parameters.save_file_name.c_str(),
+                    sl_err_to_string(save_result).c_str());
+            return 1;
+        }
     }
 
     if (capture_parameters.publish_pose)
@@ -355,7 +553,8 @@ int main(int argc, char** argv)
         sl::ERROR_CODE position_tracking_err_code = init_positional_tracking(capture_parameters);
         if (position_tracking_err_code != sl::ERROR_CODE::SUCCESS)
         {
-            ROS_FATAL("Could not initialize positional tracking. Init function returns %d.", static_cast<int>(position_tracking_err_code));
+            ROS_FATAL("Could not initialize positional tracking. Init function returns %s.", 
+                    sl_err_to_string(position_tracking_err_code).c_str());
 
             if (g_camera.isPositionalTrackingEnabled())
             {
@@ -405,6 +604,11 @@ int main(int argc, char** argv)
     if (capture_parameters.publish_pose && g_camera.isPositionalTrackingEnabled())
     {
         g_camera.disablePositionalTracking();
+    }
+
+    if (capture_parameters.save_file_name.size() > 0)
+    {
+        g_camera.disableRecording();
     }
 
     if (g_camera.isOpened())
